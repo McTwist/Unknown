e749ccc210286127a0373760b29b503d6c3ad26f
McTwist <rajb89@hotmail.com>
2016-01-04 10:49:50 +0100

    Now adds opponent actions to history

--- src/opponent.cpp
+++ src/opponent.cpp
@@ -1,18 +1,25 @@
 #include "opponent.hpp"
 
+#include "game.hpp"
 
 // Update current armies in regions owned
 void Opponent::onOpponentPlaceArmies(const std::string & name, int region, int amount)
 {
-	(void)name;
-	(void)region;
-	(void)amount;
+	// Avoid it being called more than once
+	if (GetName() != name)
+		return;
+
+	g_game->GetHistory()->AddPlacement(region, amount);
 }
 
-void Opponent::onOpponentAttackTransfer(const std::string & name, int source_region, int target_region,int amount)
+void Opponent::onOpponentAttackTransfer(const std::string & name, int source_region, int target_region, int amount)
 {
-	(void)name;
-	(void)source_region;
-	(void)target_region;
-	(void)amount;
+	// Avoid it being called more than once
+	if (GetName() != name)
+		return;
+
+	g_game->GetHistory()->AddMovement(
+		g_game->GetMap()->GetRegion(source_region),
+		g_game->GetMap()->GetRegion(target_region),
+		amount);
 }
